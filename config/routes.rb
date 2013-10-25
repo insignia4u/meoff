6e4736c8ce553996015bc8f2dfa6030a430acdd6
Hugo Svoboda <hugosvoboda@gmail.com>
2013-10-25 12:46:11 -0300

    [Invite Users]
    - confirmation user invitation by email added.
    - user sign out added in routes.

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Meoff::Application.routes.draw do
 
   devise_for :users, :controllers => { :invitations => 'users/invitations' }
 
+  devise_scope :user do
+    get "/users/sign_out", :to => "devise/sessions#destroy"
+  end
+
   resources :notifications
 
   get '/alert-the-team',	to: 'notifications#new'
