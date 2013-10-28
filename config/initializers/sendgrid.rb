8e46f989647a9026d087656de37968c3b347ff52
Hugo Svoboda <hugosvoboda@gmail.com>
2013-10-28 16:51:24 -0300

    [Heroku Deployment]
     - Heroku Sendgrid settings configured only for production env.

--- config/initializers/sendgrid.rb
+++ config/initializers/sendgrid.rb
@@ -1,9 +1,11 @@
-ActionMailer::Base.smtp_settings = {
-  :user_name => ENV['SENDGRID_USERNAME'],
-  :password => ENV['SENDGRID_PASSWORD'],
-  :domain => 'http://meoff.herokuapp.com/',
-  :address => 'smtp.sendgrid.net',
-  :port => 587,
-  :authentication => :plain,
-  :enable_starttls_auto => true
-}
+if Rails.env.production?
+  ActionMailer::Base.smtp_settings = {
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => 'http://meoff.herokuapp.com/',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
+end
